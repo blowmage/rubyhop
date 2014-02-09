f3e1bf0ccdc25ec0a51e1ba5c412271efa0925c8
Mike Moore <mike@blowmage.com>
2014-02-09 12:54:14 -0800

    Add title level
    This first screen will explain how to play the game.

--- lib/rubyhop.rb
+++ lib/rubyhop.rb
@@ -183,6 +183,69 @@ class HopLevel
   end
 end
 
+class TitleLevel
+  attr_accessor :window
+  def initialize window
+    @window = window
+    @rubyguy = Gosu::Image.new @window, get_my_file("rubyguy.png")
+
+    create_image!
+
+    # Add callback holders
+    @continue_callbacks = []
+    @quit_callbacks = []
+  end
+
+  def create_image!
+    @msg = Gosu::Image.from_text @window,
+                                "Stay alive by hopping!\n" +
+                                "Press SPACE to hop!\n" +
+                                "Press ESCAPE to close.",
+                                Gosu::default_font_name, 24
+    @msg_x = @window.width/2 - @msg.width/2
+    @msg_y = @window.height * 2 / 3
+  end
+
+  def on_continue &block
+    @continue_callbacks << block
+  end
+
+  def on_quit &block
+    @quit_callbacks << block
+  end
+
+  def continue!
+    @continue_callbacks.each { |c| c.call }
+  end
+
+  def quit!
+    @quit_callbacks.each { |c| c.call }
+  end
+
+  def start!
+    create_image!
+  end
+
+  def update
+    quit!     if @window.button_down? Gosu::KbEscape
+    continue! if ( @window.button_down?(Gosu::KbSpace)  ||
+                   @window.button_down?(Gosu::KbReturn) ||
+                   @window.button_down?(Gosu::KbEnter)  )
+  end
+
+  def draw
+    c = Math.cos(@window.time*4)
+    @rubyguy.draw_rot(((@window.width)/2), ((@window.height)/2 - 80), 1, 0,
+                      0.5, 0.5, 1.0+c*0.1, 1.0+c*0.1)
+    s = Math.sin @window.time
+    @msg.draw_rot( ((@window.width)/2 + (100*(s)).to_i),
+                    ((@window.height)/2 + 160 + s*s*s.abs*50),
+                    1, s*5, 0.5, 0.5,
+                    1.0+(0.1*s*s*s.abs), 1.0+(0.1*s*s*s.abs),
+                    Gosu::Color::RED )
+  end
+end
+
 class FailLevel
   attr_accessor :window
   def initialize window
@@ -260,16 +323,25 @@ class RubyhopGame < Gosu::Window
     @score = @high_score = 0
 
     # Levels
-    @hop  = HopLevel.new self
-    @fail = FailLevel.new self
+    @title = TitleLevel.new self
+    @hop   = HopLevel.new   self
+    @fail  = FailLevel.new  self
 
-    @hop.on_fail     { fail! }
-    @hop.on_quit     { close }
+    @title.on_continue { play! }
+    @title.on_quit     { close }
 
-    @fail.on_continue { play! }
-    @fail.on_quit     { close }
+    @hop.on_fail       { fail! }
+    @hop.on_quit       { close }
 
-    play!
+    @fail.on_continue  { play! }
+    @fail.on_quit      { close }
+
+    title!
+  end
+
+  def title!
+    @level = @title
+    @level.start!
   end
 
   def play!

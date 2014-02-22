e14251d2ee80b2c37e973041e8c37ffee74e6b5d
Davy Stevenson <davy.stevenson@gmail.com>
2014-02-22 01:43:31 -0800

    Adding initial tests for Hoop

--- test/test_rubyhop.rb
+++ test/test_rubyhop.rb
@@ -98,3 +98,23 @@ class TestPlayer < MiniTest::Test
     assert_equal true, @player.offscreen?
   end
 end
+
+class TestHoop < MiniTest::Test
+
+  def setup
+    @hoop = Hoop.new
+  end
+
+  def test_initialize
+    assert_equal 0, @hoop.x
+    assert_equal 0, @hoop.y
+    assert_equal true, @hoop.active
+  end
+
+  def test_update
+    @hoop.update 10
+    assert_equal -10, @hoop.x
+    assert_equal 0, @hoop.y
+  end
+
+end
